1458b96ed10110f19203b5d832e5d36b3e479698
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2021-03-14 18:51:38 +0530

    Added logo and text on blocker

--- blocker.js
+++ blocker.js
@@ -7,10 +7,25 @@ let tobeblocked = false;
 
 const blocker = document.createElement("div");
 blocker.className = "blocker";
+const logoContainer = document.createElement("div");
+logoContainer.className = "logo-container";
+const logo = document.createElement("img");
+logo.className = "logo";
+// logo.src = chrome.runtime.getURL("/assets/masked-logo.svg");
+logo.src =
+    "https://dl3.pushbulletusercontent.com/DHT4I1JS8jT3OeHbtgW94ecLyo5XvegT/masked-logo.svg";
+logoContainer.appendChild(logo);
+blocker.appendChild(logoContainer);
+const textContainer = document.createElement("div");
+textContainer.className = "text-container";
+textContainer.innerText =
+    "This video contains unethical content. Click to find out more.";
+blocker.appendChild(textContainer);
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.blockContent === true) {
         tobeblocked = true;
-        console.log("EXTENSION IS ACTIVE");
+        console.log("MASKED EXTENSION IS ACTIVE");
         const videoPlayer = document.querySelector("video");
         videoPlayer.pause();
 
@@ -34,9 +49,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                     bottom,
                     left,
                 } = videoPlayer.getBoundingClientRect();
-                // console.log(videoPlayer, "Resetting blocker...", blocker);
-                blocker.style.width = `${width}px`;
-                blocker.style.height = `${height}px`;
+                blocker.style.width = `${width + 5}px`;
+                blocker.style.height = `${height + 5}px`;
                 blocker.style.top = `${top}px`;
                 blocker.style.left = `${left}px`;
                 videoPlayer.pause();
